a2e8864879f5a939d144eca29c6d930371f5fdc6
jviboch-kf <john.viboch@gmail.com>
2025-02-25 17:27:02 -0500

    Update AI_ROLE

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -3,4 +3,4 @@ export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
 export const AI_NAME: string = `[AI NAME]`;
 export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_ROLE: string = `**An AI-powered sales assistant** that helps sales managers *handle objections*, *generate sales emails*, and *provide training support* **to close more deals**.`;
