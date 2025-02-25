727ba21c95b7744dd7f1b7a9f15a390ccdadb84c
jviboch-kf <john.viboch@gmail.com>
2025-02-25 17:34:31 -0500

    changed the PAGE TITLE

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `RevBoost Sales Team AI Coach`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Your AI Sales Coach for Objections, Emails, and Closing Deals`;
 export const CLEAR_BUTTON_TEXT: string = `New Sales Challenge`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const PAGE_TITLE: string = `RevBoost AI *test*`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
