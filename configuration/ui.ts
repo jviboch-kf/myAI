284d0a3488cdb64d3e0612d902709b567ef54bc6
jviboch-kf <john.viboch@gmail.com>
2025-02-25 18:15:48 -0500

    updated chat header

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,6 +1,6 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `RevBoost Sales Team AI Coach`;
+export const CHAT_HEADER: string = `KF25 Sales - AI Coach`;
 export const MESSAGE_PLACEHOLDER: string = `While specific information isn't currently available, I'm here to support your sales efforts with insights on handling objections, crafting persuasive emails, and providing training strategies to help close more deals. Let's keep moving forward toward your sales success.`;
 export const FOOTER_MESSAGE: string = `Your AI Sales Coach for Objections, Emails, and Closing Deals`;
 export const CLEAR_BUTTON_TEXT: string = `New Sales Challenge`;
