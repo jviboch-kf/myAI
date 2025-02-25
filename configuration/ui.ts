f2291895c66ad017986481e30ac488a416ef162f
jviboch-kf <john.viboch@gmail.com>
2025-02-25 17:59:08 -0500

    Edited Page Title because italics isn't working properly

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `RevBoost Sales Team AI Coach`;
 export const MESSAGE_PLACEHOLDER: string = `While specific information isn't currently available, I'm here to support your sales efforts with insights on handling objections, crafting persuasive emails, and providing training strategies to help close more deals. Let's keep moving forward toward your sales success.`;
 export const FOOTER_MESSAGE: string = `Your AI Sales Coach for Objections, Emails, and Closing Deals`;
 export const CLEAR_BUTTON_TEXT: string = `New Sales Challenge`;
-export const PAGE_TITLE: string = `RevBoost AI _test_`;
+export const PAGE_TITLE: string = `RevBoost AI *Prototype*`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
