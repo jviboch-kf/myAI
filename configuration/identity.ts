df2ab1f1d57764c232cf5430f2a614c4235ddc12
jviboch-kf <john.viboch@gmail.com>
2025-02-25 16:12:15 -0500

    adding bold text
    used <b>

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,4 +1,4 @@
-export const OWNER_NAME: string = `KF25 RevBoost Inc.`;
+export const OWNER_NAME: string = `<b>KF25 RevBoost<b> Inc.`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
 export const AI_NAME: string = `RevBoost AI`;
